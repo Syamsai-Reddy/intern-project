8bf1d44547c8c1c66e9389ca0318d613bc0e4253
Syamsai Reddy <syamsaireddyyeturu@gmail.com>
2024-07-09 08:07:15 +0530

    Initial commit

---
+++ Desktop/nxtjs/shopping-cart/src/app/loading.js
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+function Loading() {
+  return <Skeleton className="w-[400px] h-[400px] text-black text-3xl " > loading</Skeleton>;
+}
+
+export default Loading;
